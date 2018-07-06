48717e458f483fd741c2c726d60ac978d34e39d0
Andy Dingler <andy.dingler@comcast.net>
2018-07-06 10:34:36 -0700

    created function to pull historical salaries and ftps

---
+++ dfs_scraper.py
@@ -0,0 +1,58 @@
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
+from random import randint
+import pandas as pd
+import os
+import io
+
+
+encoding = 'ISO-8859-1'
+
+
+def soup_scrape(url):
+    base_dir = "page_cache/"
+    if not os.path.exists(base_dir):
+        os.makedirs(base_dir)
+    url_hash = url.replace("/", "").replace(":", "").replace("?", "").replace(".", "")
+    try:
+        with open(base_dir+url_hash, "r") as file_read:
+            return BeautifulSoup(file_read.read(), "html.parser")
+    except Exception:
+        print(url)
+        sleep(randint(2, 7))
+        html_data = requests.get(url,
+                                 headers={"User-Agent":
+                                              "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, "
+                                              "like Gecko) Chrome/58.0.3029.110 Safari/537.36"}).text
+        soup_data = BeautifulSoup(html_data, "html.parser")
+        with open(base_dir+url_hash, "w") as file_write:
+            file_write.write(html_data)
+        return soup_data
+    except RuntimeError:
+        print(RuntimeError)
+
+
+def update_results(years, weeks,
+                   previous_dfs_results,
+                   prev_results):
+    if 'DK' in previous_dfs_results:
+        base_url = "http://rotoguru1.com/cgi-bin/fyday.pl?game=dk&scsv=1&week=WEEK&year=YEAR"
+    else:
+        base_url = "http://rotoguru1.com/cgi-bin/fyday.pl?game=fd&scsv=1&week=WEEK&year=YEAR"
+
+    if prev_results is False:
+        prev_dfs_results_df = pd.DataFrame()
+    else:
+        prev_dfs_results_df = pd.read_csv(previous_dfs_results)
+
+    all_games = pd.DataFrame()
+    for yr in years:
+        for wk in weeks:
+            soups = soup_scrape(base_url.replace("WEEK", wk).replace("YEAR", yr))
+            all_games = pd.concat([all_games, pd.read_csv(io.StringIO(soups.find("pre").text), sep=";")])
+
+    prev_dfs_results_df = pd.concat((prev_dfs_results_df, all_games)).drop_duplicates().drop_duplicates()
+    prev_dfs_results_df.to_csv(previous_dfs_results, index=False)
+
+    return prev_dfs_results_df
